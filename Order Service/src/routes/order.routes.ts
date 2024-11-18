ea7e887b053a6d28fe22ea4ce532c6d4736e43c2
Fadhil <fadhilanugrah21@gmail.com>
2024-11-18 15:40:47 +0700

    feat: Add order and order line items tables with relationships and migration scripts

--- Order Service/src/routes/order.routes.ts
+++ Order Service/src/routes/order.routes.ts
@@ -9,24 +9,24 @@ router.post(
   "/order",
   async (req: Request, res: Response, next: NextFunction) => {
     //  3rd step: publish the message
-    await MessageBroker.publish({
-      topic: "OrderEvents",
-      headers: { token: req.headers.authorization },
-      event: OrderEvent.CREATE_ORDER,
-      message: {
-        orderId: 1,
-        items: [
-          {
-            productId: 1,
-            quantity: 2,
-          },
-          {
-            productId: 2,
-            quantity: 3,
-          },
-        ],
-      },
-    });
+    // await MessageBroker.publish({
+    //   topic: "OrderEvents",
+    //   headers: { token: req.headers.authorization },
+    //   event: OrderEvent.CREATE_ORDER,
+    //   message: {
+    //     orderId: 1,
+    //     items: [
+    //       {
+    //         productId: 1,
+    //         quantity: 2,
+    //       },
+    //       {
+    //         productId: 2,
+    //         quantity: 3,
+    //       },
+    //     ],
+    //   },
+    // });
     return res.status(200).json({ message: "Order created" });
   }
 );
